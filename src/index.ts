71072fccf090159432fbd3a174553a2c922f49b9
Victor Inácio <chustrupgamer@gmail.com>
2021-11-16 16:31:45 -0300

    class Canvas created and it can access canvas element and its context

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import './assets/style.css';
+
+class Canvas {
+    private canvasEl: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+
+    constructor(element: HTMLCanvasElement) {
+        this.canvasEl = element;
+        this.context = element.getContext('2d');
+    }
+}
+
+const canvasEl = document.querySelector('canvas');
+const canvas = new Canvas(canvasEl);
