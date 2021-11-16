8d16a30f521bb30d1a91c996b9dd54d12c66cfe4
Victor Inácio <chustrupgamer@gmail.com>
2021-11-16 16:42:26 -0300

    movePlayer method working

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,27 @@ class Canvas {
     private canvasEl: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
 
+    private player = {
+        x: 590,
+        y: 590,
+        size: 10,
+    };
+
     constructor(element: HTMLCanvasElement) {
         this.canvasEl = element;
         this.context = element.getContext('2d');
+
+        this.context.fillRect(
+            this.player.x,
+            this.player.y,
+            this.player.size,
+            this.player.size
+        );
+    }
+
+    private movePlayer(x, y) {
+        this.player.x = x - (x % this.player.size);
+        this.player.y = y - (y % this.player.size);
     }
 }
 
