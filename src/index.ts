5db3ad5ad45bd0353dab379c2009475ecbbdb63c
Victor Inácio <chustrupgamer@gmail.com>
2021-11-16 19:29:43 -0300

    Keyboard listener ready

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
 import './assets/style.css';
 
+interface Listener {
+    code: string;
+    callback: Function;
+}
 
+class KeyboardListener {
+    private listeners: Array<Listener> = [];
+
+    constructor() {
+        document.addEventListener('keydown', (event: KeyboardEvent) => {
+            this.handleKeyDown(event);
+        });
+    }
+
+    private handleKeyDown(e: KeyboardEvent): void {
+        this.listeners.forEach((listener) => {
+            if (e.code === listener.code) listener.callback();
+        });
+    }
+
+    public on(code: string, callback: Function): void {
+        this.listeners.push({
+            code,
+            callback,
+        });
+    }
+}
 
 class Sneak {
     private x: number;
