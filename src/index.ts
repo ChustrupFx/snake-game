6c8938ae074683250dabc286b9f030ec21f7913b
Victor Inácio <chustrupgamer@gmail.com>
2021-11-16 18:13:06 -0300

    Created method to update canvas and draw lines

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,31 @@ import './assets/style.css';
 class Canvas {
     private canvasEl: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
+    private width: number = 600;
+    private height: number = 600;
 
     private player = {
-        x: 590,
-        y: 590,
-        size: 10,
+        x: 50,
+        y: 50,
+        size: 15,
     };
 
     constructor(element: HTMLCanvasElement) {
         this.canvasEl = element;
+        this.canvasEl.width = this.width;
+        this.canvasEl.height = this.height;
+
         this.context = element.getContext('2d');
+
+        setInterval(() => {
+            this.update();
+        }, 100);
+    }
+
+    private update(): void {
+        this.clearCanvas();
+        this.drawLines();
+        this.movePlayer(this.player.x + this.player.size, this.player.y);
 
         this.context.fillRect(
             this.player.x,
@@ -22,11 +37,45 @@ class Canvas {
         );
     }
 
-    private movePlayer(x, y) {
+    private clearCanvas(): void {
+        this.context.clearRect(0, 0, this.width, this.height);
+    }
+
+    private movePlayer(x: number, y: number): void {
         this.player.x = x - (x % this.player.size);
         this.player.y = y - (y % this.player.size);
     }
+
+    private drawLines() {
+        const numberOfLines = this.width / this.player.size;
+
+        this.context.strokeStyle = 'gray';
+        this.context.lineWidth = 2;
+
+        this.drawHorizontalLines(numberOfLines);
+        this.drawVerticalLines(numberOfLines);
+    }
+
+    private drawHorizontalLines(numberOfLines) {
+        for (let i = 0; i <= numberOfLines; i++) {
+            this.context.beginPath();
+            this.context.moveTo(0, i * this.player.size);
+            this.context.lineTo(this.width, i * this.player.size);
+            this.context.stroke();
+            this.context.closePath();
+        }
+    }
+
+    private drawVerticalLines(numberOfLines) {
+        for (let i = 0; i <= numberOfLines; i++) {
+            this.context.beginPath();
+            this.context.moveTo(i * this.player.size, 0);
+            this.context.lineTo(i * this.player.size, this.height);
+            this.context.stroke();
+            this.context.closePath();
+        }
+    }
 }
 
-const canvasEl = document.querySelector('canvas');
+const canvasEl: HTMLCanvasElement = document.querySelector('canvas');
 const canvas = new Canvas(canvasEl);
