77b13b46c44412298cab343e793811198923b8be
Victor Inácio <chustrupgamer@gmail.com>
2021-11-17 21:56:54 -0300

    ColliderDetector class created

---
+++ src/ColliderDetector.ts
@@ -0,0 +1,26 @@
+import Fruit from './Fruit';
+import Sneak from './Sneak';
+
+class ColliderDetector {
+    private fruit: Fruit;
+    private snake: Snake;
+
+    constructor(snake: Snake, fruit: Fruit) {
+        this.fruit = fruit;
+        this.snake = snake;
+    }
+
+    public collisionDetected() {
+        return this.isSameXAxisValue() && this.isSameYAxisValue();
+    }
+
+    private isSameXAxisValue(): boolean {
+        return this.fruit.getX() === this.snake.getX();
+    }
+
+    private isSameYAxisValue(): boolean {
+        return this.fruit.getY() === this.snake.getY();
+    }
+}
+
+export default ColliderDetector;
